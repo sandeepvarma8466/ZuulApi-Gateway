e18abae18459e9d1d9836f5c2a583b7ac9f41e77
sandeepvarma8466 <svarma852@gmail.com>
2022-09-06 17:44:58 +0530

    LMS ZullApi Gateway

---
+++ src/test/java/com/blz/demo/NetflixZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
